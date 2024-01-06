27839c1c7501a07a8f37538250d75e078119ba0b
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2024-01-07 05:00:29 +0600

    custom_widgets

--- lib/features/search/presentation/pages/search/search_page.dart
+++ lib/features/search/presentation/pages/search/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:eco_market_app/config/config.dart';
+import 'package:eco_market_app/features/search/presentation/widgets/custom_button_widget.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -11,6 +12,8 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,21 +21,128 @@ class _SearchPageState extends State<SearchPage> {
           title: const Text("Продукты"),
         ),
         body: Padding(
-          padding: EdgeInsets.only(right: 16, top: 12, left: 16),
+          padding: const EdgeInsets.only(right: 16, top: 12, left: 16),
           child: Column(
             children: [
               TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    constraints: BoxConstraints(
+                    contentPadding: const EdgeInsets.all(10.0),
+                    constraints: const BoxConstraints(
                       maxHeight: 40,
                     ),
                     filled: true,
                     fillColor: AppColors.lightGrey,
                     hintText: "Быстрый поиск",
-                    hintStyle: TextStyle(
-                      color: AppColors.grey,
+                    hintStyle: const TextStyle(
+                        color: AppColors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                    prefixIcon: const Icon(Icons.search, color: AppColors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      borderSide: BorderSide.none,
                     )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: 38,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                          onTap: () {},
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: _currentIndex == index
+                                  ? AppColors.green
+                                  : AppColors.white,
+                              border: Border.all(
+                                color: _currentIndex == index
+                                    ? AppColors.white
+                                    : AppColors.grey,
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                              child: Text(
+                                "data",
+                                style: TextStyle(
+                                  color: _currentIndex == index
+                                      ? AppColors.white
+                                      : AppColors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ));
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                          width: 8,
+                        ),
+                    itemCount: 7),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 11.0,
+                      mainAxisSpacing: 11.0,
+                      mainAxisExtent: 284,
+                    ),
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            color: AppColors.lightGrey,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Center(
+                                    child: Image.asset(
+                                      "assets/images/product.png",
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  const Text(
+                                    "data",
+                                    style: TextStyle(
+                                      color: AppColors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24),
+                                  const Text(
+                                    "data",
+                                    style: TextStyle(
+                                      color: AppColors.green,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 16),
+                                  CustomButtonWidget(
+                                      text: "Добавить",
+                                      height: 32,
+                                      onPressed: () {})
+                                ]),
+                          ),
+                        ),
+                      );
+                    }),
               )
             ],
           ),
---
+++ lib/features/search/presentation/widgets/custom_button_widget.dart
@@ -0,0 +1,35 @@
+import 'package:eco_market_app/config/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomButtonWidget extends StatelessWidget {
+  const CustomButtonWidget(
+      {super.key,
+      required this.text,
+      required this.height,
+      required this.onPressed});
+  final String text;
+  final double? height;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: double.infinity,
+      child: ElevatedButton(
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0))),
+              backgroundColor: const MaterialStatePropertyAll(AppColors.green)),
+          onPressed: onPressed,
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          )),
+    );
+  }
+}
