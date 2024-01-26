81acd977d798c115f9738123509d68aea8f5b94a
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2024-01-26 23:11:27 +0600

    CartScreen - API

---
+++ lib/features/cart/presentation/cubit/cubit/cart_screen_state.dart
@@ -0,0 +1,10 @@
+part of 'cart_screen_cubit.dart';
+
+sealed class CartScreenState extends Equatable {
+  const CartScreenState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class CartScreenInitial extends CartScreenState {}
